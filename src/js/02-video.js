8e0d01c4effd5c93e2d539e744f5dde5bed1311a
AlrMel <alexandrm1233@gmail.com>
2023-07-29 15:55:30 +0300

    Update_02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const VIDEO_CURRENT_TIME = 'videoplayer-current-time';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const onPlay = function (event) {
+  localStorage.setItem(VIDEO_CURRENT_TIME, event.seconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player.setCurrentTime(JSON.parse(localStorage.getItem(VIDEO_CURRENT_TIME)));
